89da13366d6c584b6bddef78b90959bddbaa8e95
landlordSLi <woshilsinhl@gmail.com>
2020-04-20 22:16:26 +0800

    all   files upload 上传所有的文件

--- src/main/java/com/ls/share/shareforks/sharing/Bizsk.java
+++ src/main/java/com/ls/share/shareforks/sharing/Bizsk.java
@@ -12,6 +12,6 @@ public class Bizsk {
     public static void main(String[] args) {
         System.out.println("he咯都是来的来的");
         System.out.println("的肯定快0000点快点");
-
+        System.out.println("这是controller0");
     }
 }
